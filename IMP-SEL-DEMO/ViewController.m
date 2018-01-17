73112de79705cd2f58dd03494a9e42875d63ff78
Yggdrasill-7C9 <384008234@qq.com>
2018-01-17 17:57:49 +0800

    第一次提交

--- IMP-SEL-DEMO/ViewController.m
+++ IMP-SEL-DEMO/ViewController.m
@@ -10,20 +10,60 @@
 
 @interface ViewController ()
 
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor redColor];
+//    [self test:nil];
+//    [self test1:nil];
+    
+    
+    //获取方法 ID
+    SEL methodID = @selector(test:);
+    
+    //通过 @selector 访问这个方法
+    [self performSelector:methodID withObject:nil];//容易引起泄露
+    
+    //获取方法名
+    NSString* methodName = NSStringFromSelector(methodID);
+    NSLog(@"methodName:%@",methodName);
+    
+    //通过 IMP 访问这个方法
+    IMP methodPoint = [self methodForSelector:methodID];
+    methodPoint();
+
+
+    int func(int count);
+    int (*funcP)(int count) = &func;
+    int result1 = func(10);
+    int result2 = (*funcP)(10);
+    NSLog(@"调用函数%d %d",result1,result2);
+    
+
 }
 
+int func(int count){
+    
+    return ++count;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)test:(NSString*)str{
+    
+    static NSInteger i = 0;
+    i++;
+    NSLog(@"%s调用%s方法%ld次",object_getClassName(self),__FUNCTION__,i);
+}
+
 
 
 @end
